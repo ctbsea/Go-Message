186e94392f8f5f29228b608ca0dceac5f3cc2a66
ctb <ctb>
2020-04-02 12:55:28 +0800

    'debug'

--- gateway/pprof.go
+++ gateway/pprof.go
@@ -9,4 +9,8 @@ func NewPprof(app *iris.Application)  {
 	p := pprof.New()
 	app.Any("/debug/pprof", p)
 	app.Any("/debug/pprof/{action:path}", p)
+	app.Any("/debug/pprof/cmdline", p)
+	app.Any("/debug/pprof/profile", p)
+	app.Any("/debug/pprof/trace", p)
+	app.Any("/debug/pprof/symbol", p)
 }
